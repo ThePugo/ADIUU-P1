4798f58d3df34d65959604207dec8e33a8cd4867
ThePugo <95140143+ThePugo@users.noreply.github.com>
2023-10-28 11:59:10 +0200

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,1 +1,23 @@
+CREATE TABLE CPU (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    image VARCHAR(255),
+    url VARCHAR(255),
+    brand VARCHAR(255),
+    socket VARCHAR(255),
+    speed DECIMAL(10, 2),
+    coreCount INT,
+    threadCount INT,
+    power INT
+);
 
+CREATE TABLE GPU (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    image VARCHAR(255),
+    url VARCHAR(255),
+    brand VARCHAR(255),
+    VRAM INT,
+    resolution VARCHAR(255),
+    power INT
+);
